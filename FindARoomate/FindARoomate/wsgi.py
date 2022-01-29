65cdf79df5b5a945e4306598bd08d1edd16056c5
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-01-29 13:07:58 +0100

    Change

--- FindARoomate/FindARoomate/wsgi.py
+++ FindARoomate/FindARoomate/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindARoomate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindARoomate.settings.dev')
 
 application = get_wsgi_application()
