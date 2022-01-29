104cb075b5a42f452275b05075a25fd390a3e2df
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-01-29 14:42:10 +0100

    Chore: Fixed project structure

--- FindARoomate/wsgi.py
+++ FindARoomate/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindARoomate.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindARoomate.settings')
 
 application = get_wsgi_application()
