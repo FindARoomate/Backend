543885207caf082687cb7960341e4352e4cb5770
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-03-13 00:47:29 +0100

    FIX: STARTTLS extension not supported by server error

--- FindARoomate/settings.py
+++ FindARoomate/settings.py
@@ -156,13 +156,13 @@ DJOSER = {
     },
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
 
 # Internationalization
