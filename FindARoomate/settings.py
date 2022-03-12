e943c8997d0e5a6401b1ebea6a4b5e1bdbcf386e
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-03-12 23:51:20 +0100

    Chore: removed tests

--- FindARoomate/settings.py
+++ FindARoomate/settings.py
@@ -158,6 +158,8 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_USE_TLS = True
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
